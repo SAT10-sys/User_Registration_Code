261ec3ea1fee8bf2586d4c53bc2ff4d337bcbcbb
Satantra Tewari <satantra1508@gmail.com>
2020-10-20 17:44:19 +0530

    [satantra] welcome message displayed

---
+++ User_Registration_Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace User_Registration_Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Program");
+        }
+    }
+}
