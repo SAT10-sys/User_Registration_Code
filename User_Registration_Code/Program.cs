ac0bd4c05d656f510bb1e7453e4b31003fd949ca
Satantra Tewari <satantra1508@gmail.com>
2020-10-20 17:54:01 +0530

    [satantra] added method to validate first name

--- User_Registration_Code/Program.cs
+++ User_Registration_Code/Program.cs
@@ -7,6 +7,18 @@ namespace User_Registration_Code
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Program");
+            ValidatePattern validatePattern = new ValidatePattern();
+        RegStart1:
+            Console.WriteLine("Enter first name(should begin with capital letters and have minimum 3 letters)");
+            string firstName = Console.ReadLine();
+            bool checkFirstName = validatePattern.ValidateFirstName(firstName);
+            if(checkFirstName==true)
+                Console.WriteLine("FIRST NAME: "+firstName);
+            else
+            {
+                Console.WriteLine("Please enter valid first name");
+                goto RegStart1;
+            }
         }
     }
 }
---
+++ User_Registration_Code/ValidatePattern.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace User_Registration_Code
+{
+    public class ValidatePattern
+    {
+        public string FIRST_NAME = "^[A-Z][A-Za-z]{2,}$";
+        public bool ValidateFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, FIRST_NAME);
+        }
+    }
+}
